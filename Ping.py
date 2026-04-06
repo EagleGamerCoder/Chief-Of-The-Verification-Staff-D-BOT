fe9f750638d70b3bc542c8a7fbad400dbe469b32
EagleGamerCoder <natedennis001@gmail.com>
2026-04-06 16:02:13 +0100

    Update Ping.py

--- Ping.py
+++ Ping.py
@@ -8,9 +8,10 @@ def home():
     return "Bot is running!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    import os
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
-    t.daemon = True
     t.start()
